9be0d6e11687ab4d9b45f5098aefed5ccce20629
Sn4u <Sn4u@github.com>
2021-12-01 15:22:38 +0000

    Solved Day 1

--- Day 1/day_1.py
+++ Day 1/day_1.py
@@ -1,7 +1,22 @@
+with open("input", "r") as f:
+    file = list(map(int, f.read().splitlines()))
+
 
 def one():
-    pass
+    count = 0
+    for i in range(len(file)):
+        try:
+            if file[i] < file[i+1]:
+                count += 1
+        except IndexError:
+            pass
+    print(count)
 
 
 def two():
-    pass
+    count = 0
+    for i in range(len(file)-3):
+        if sum(file[i:i+3]) < sum(file[i+1:i+4]):
+            count += 1
+
+    print(count)
