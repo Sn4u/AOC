ddb507f0480f97404f03bb70ee26ab02192abfb1
Sn4u <Sn4u@github.com>
2021-12-02 15:05:12 +0000

    Solved Day 2

--- Day 2/day_2.py
+++ Day 2/day_2.py
@@ -1,7 +1,31 @@
+with open("input", "r") as f:
+    file = list(f.read().splitlines())
+
 
 def one():
-    pass
+    dy = dx = 0
+    for line in file:
+        direction, distance = line.split()
+        distance = int(distance)
+        if direction == "forward":
+            dx += distance
+        elif direction == "down":
+            dy += distance
+        elif direction == "up":
+            dy -= distance
+    print(dy * dx)
 
 
 def two():
-    pass
+    dy = dx = aim = 0
+    for line in file:
+        direction, distance = line.split()
+        distance = int(distance)
+        if direction == "forward":
+            dx += distance
+            dy += (aim*distance)
+        elif direction == "down":
+            aim += distance
+        elif direction == "up":
+            aim -= distance
+    print(dy * dx)
