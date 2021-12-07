0d7c9f8585f9affc7e2cade865c04ebb542b9d20
Sn4u <Sn4u@github.com>
2021-12-07 14:39:25 +0000

    Solved day 7

--- Day 7/day_7.py
+++ Day 7/day_7.py
@@ -1,7 +1,21 @@
+from statistics import median
+
+with open("input", "r") as f:
+    file = list(map(int, f.read().split(",")))
+
 
 def one():
-    pass
+    med = int(median(file))
+    cost = sum(map(lambda x: abs(med - x), file))
+    print(cost)
 
 
 def two():
-    pass
+    def cost(distance):
+        return distance * (distance + 1) // 2
+
+    fuels = set()
+    for i in range(min(file), max(file)):
+        fuels.add(sum(cost(abs(crab - i)) for crab in file))
+
+    print(min(fuels))
