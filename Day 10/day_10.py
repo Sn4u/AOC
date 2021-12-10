da4ccec7d6a3f57f0808c42882361f6b81ca43e7
Sn4u <Sn4u@github.com>
2021-12-10 17:03:44 +0000

    Solved day 10

--- Day 10/day_10.py
+++ Day 10/day_10.py
@@ -1,7 +1,76 @@
+with open("input", "r") as f:
+    file = f.read().splitlines()
+
+scores = {
+    ")": 3,
+    "]": 57,
+    "}": 1197,
+    ">": 25137,
+}
+
+other_score = {
+    "(": 1,
+    "[": 2,
+    "{": 3,
+    "<": 4
+}
+
+match = {"(": ")",
+         "[": "]",
+         "{": "}",
+         "<": ">"}
+
 
 def one():
-    pass
+    total = 0
+
+    def p():
+        while True:
+            for i in range(len(line)):
+                try:
+                    if match[line[i]] == line[i + 1]:
+                        line.pop(i), line.pop(i)
+                        print("".join(line))
+                        break
+                except KeyError:
+                    return scores[line[i]]
+
+                except IndexError:
+                    return 0
+
+    for line in file:
+        line = list(line)
+        total += p()
+    print(total)
 
 
 def two():
-    pass
+    totals = []
+
+    def k():
+        while True:
+            for i in range(len(line)):
+                try:
+                    if match[line[i]] == line[i + 1]:
+                        line.pop(i), line.pop(i)
+                        break
+                except KeyError:
+                    return
+
+                except IndexError:
+                    return line
+
+    def compute_score(completed):
+        score = 0
+        for c in reversed(completed):
+            score *= 5
+            score += other_score[c]
+        return score
+
+    for line in file:
+        line = list(line)
+        line = k()
+        if line is None:
+            continue
+        totals.append(compute_score(line))
+    print(sorted(totals)[len(totals)//2])
